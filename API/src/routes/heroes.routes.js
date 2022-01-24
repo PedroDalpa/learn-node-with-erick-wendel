a894c9bca01c5c051c1415d83e37c0e2ae6f5373
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-01-24 17:11:22 -0300

    alter hap version add add verification

--- API/src/routes/heroes.routes.js
+++ API/src/routes/heroes.routes.js
@@ -1,5 +1,5 @@
 const BaseRoutes = require('./index.routes')
-
+const Joi = require('joi');
 
 class HeroRoutes extends BaseRoutes {
   constructor(db) {
@@ -11,18 +11,25 @@ class HeroRoutes extends BaseRoutes {
     return {
       path: '/heroes',
       method: 'GET',
+      options: {
+        validate: {
+          query: Joi.object({
+            skip: Joi.number().integer().default(0),
+            limit: Joi.number().integer().default(10),
+            name: Joi.string().min(3).max(100)
+          }).options({ stripUnknown: true }),
+          failAction: (request, headers, erro) => {
+            throw erro
+          }
+        }
+      },
       handler: (request, headers) => {
         try {
           const { skip, limit, name } = request.query;
-          let query = {};
 
-          if (name) query.name = name;
+          const query = name ? { name: { $regex: `.*${name}*.` } } : {};
 
-          if ((isNaN(skip) && skip) || (isNaN(limit) && limit)) {
-            throw new Error('Tipo invalido')
-          }
-
-          return this.db.read(query, Number(skip), Number(limit));
+          return this.db.read(query, skip, limit);
         } catch (error) {
           console.error(error);
 
